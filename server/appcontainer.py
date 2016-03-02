ce26a413eca6ad7d6ddddc6f1f9a31393460f531
jonkr <jonkronander@jkmbpr.local>
2016-03-02 20:23:43 +0100

    Disables stack sample polling

--- server/appcontainer.py
+++ server/appcontainer.py
@@ -1,10 +1,8 @@
 from app import create_app
 import config
-import stacksampler
 
 
 app = create_app()
-profile_server_thread = stacksampler.run_profiler()
 
 if __name__ == '__main__':
     app.run(port=5000, debug=config.DEBUG, host='0.0.0.0')
