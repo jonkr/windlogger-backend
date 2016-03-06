1e8cfcd37146c649d1e1c5e4301864ed5ce89322
jonkr <jonkronander@jkmbpr.local>
2016-03-06 14:15:38 +0100

    Adds logging middleware: Morgan

--- server2/app.js
+++ server2/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const routes = require('./routes');
 const app = express();
 
 app.use('/', routes);
+
+app.use(morgan('combined'));
 
 app.use(function(err, req, res, next) {
 	console.log('Detected error:', err);
